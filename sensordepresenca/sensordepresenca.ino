6b8ac457776aef4ccf68b92b170a0d47d90938a2
Edmiel Loiola de Oliveira <edmieloliveira@gmail.com>
2020-10-21 18:52:24 -0300

    Código do Sensor de Presença
    código ok

---
+++ sensordepresenca/sensordepresenca.ino
@@ -0,0 +1,22 @@
+/*
+* Lampada Inteligente
+* Derick Abreu Montagna e Edmiel Loiola de Oliveira
+* 
+* É um projeto proposto pela disciplina de sistemas embarcados para obter a nota da M2, de maneira, que é proposto a realização de uma lampada inteligente com sensor de presença
+* e sensor de luminosidade.
+* 21/10/2020
+*/
+const int pinoPIR = 19; //PINO DIGITAL UTILIZADO PELO SENSOR DE PRESENÇA
+const int pinoLED = 20; //PINO DIGITAL UTILIZADO PELO LED
+ 
+void setup(){
+  pinMode(pinoLED, OUTPUT); //DEFINE O PINO COMO SAÍDA
+  pinMode(pinoPIR, INPUT); //DEFINE O PINO COMO ENTRADA
+}
+void loop(){
+ if(digitalRead(pinoPIR) == HIGH){ //SE A LEITURA DO PINO FOR IGUAL A HIGH, FAZ
+    digitalWrite(pinoLED, HIGH); //ACENDE O LED
+ }else{ //SENÃO, FAZ
+  digitalWrite(pinoLED, LOW); //APAGA O LED
+ }
+}
