36b8166735c096c9640ab5d8b86c481e5c699af1
Derick <doxdank@hotmail.com>
2020-10-21 18:46:12 -0300

    main
    está com o cabeçalho

---
+++ main/main.ino
@@ -0,0 +1,18 @@
+/*
+* Lampada Inteligente
+* Derick Abreu Montagna e Edmiel Loiola de Oliveira
+* 
+* É um projeto proposto pela disciplina de sistemas embarcados para obter a nota da M2, de maneira, que é proposto a realização de uma lampada inteligente com sensor de presença
+* e sensor de luminosidade.
+* 21/10/2020
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
