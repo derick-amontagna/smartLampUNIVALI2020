78cbb3e5cf867973cf2e198579d9b8ee357f7eb2
Derick <doxdank@hotmail.com>
2020-10-23 15:50:07 -0300

    estabelecendo função
    foi feita as funções para o ldr e o sensor de presença

--- main/main.ino
+++ main/main.ino
@@ -9,36 +9,43 @@
 
 //Sensor de luz com LDR
 
-#define ledPin 21; //Pino digital para o LED
-#define ldrPin = 35; //Pino analogico para o LDR
-#define pinoPIR = 19; //Pino digital para o sensor de presença
+#define ledPin 21 //Pino digital para o LED
+#define ldrPin 35 //Pino analogico para o LDR
+#define pinoPIR 19 //Pino digital para o sensor de presença
 
 int ldrValor = 0; //Valor lido do LDR
 
  
-void setup() {
+void setup() 
+{
  pinMode(ledPin,OUTPUT); //define a ledPin como saída
  pinMode(ldrPin,INPUT); //define a ldrPin como saída
  pinMode(pinoPIR, INPUT); //DEFINE O PINO COMO ENTRADA
   
  Serial.begin(115200); //Inicia a comunicação serial
 }
- 
-void loop() {
-  
- ///LDR
+
+void verificandoLDR()
+{
+
  ldrValor = analogRead(ldrPin); //O valor lido será entre 0 e 1023
+ 
  //se o valor lido for maior ou igual que 1500, liga o led
  if (ldrValor>= 1500) digitalWrite(ledPin,HIGH);
- // senão, apaga o led
  else digitalWrite(ledPin,LOW);
- delay(100);
+  
+}
 
- ///Sensor de presença
- if(digitalRead(pinoPIR) == HIGH){ //SE A LEITURA DO PINO FOR IGUAL A HIGH, FAZ
-    digitalWrite(lenPin, HIGH); //ACENDE O LED
- }else{ //SENÃO, FAZ
-  digitalWrite(lenPin, LOW); //APAGA O LED
- }
- 
+void verificandoPresenca()
+{
+
+ if(digitalRead(pinoPIR) == HIGH) digitalWrite(ledPin, HIGH); 
+ else digitalWrite(ledPin, LOW); //APAGA O LED
+  
+}
+
+void loop() 
+{
+  
+
 }
