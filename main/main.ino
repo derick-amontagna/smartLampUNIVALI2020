90039c0bf6d428b52f9400cf4668336154dd8f57
Derick <doxdank@hotmail.com>
2020-10-26 13:48:44 -0300

    Portado para o Blynk App
    Foi trocado o gerenciador de ações do circus para o Blynk

--- main/main.ino
+++ main/main.ino
@@ -8,99 +8,96 @@
 */
 
 // Bibliotecas
-#include <CircusESP32Lib.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 
 // Definindo constante 
 #define ledPin 21 //Pino digital para o LED
 #define ldrPin 35 //Pino analogico para o LDR
 #define pinoPIR 19 //Pino digital para o sensor de presença
+#define BLYNK_PRINT Serial
+WidgetLED statusLed(V0);
 
 int ldrValor = 0; //Valor lido do LDR
 int randNumber = 0;
-int statusLed = 0;
-int modoSelecionado = 0;
+int manual_order = 0;
 
-/** Estas são as declarações relacionadas ao CircusESP32Lib**/
+/** Estas são as declarações relacionadas ao the Blynk App**/
+char auth[] = "_qtOUSD-l2L7iMNt5x955DfegMSkXSEi";
 char ssid[] = "GVT-A6E1";                         
-char password[] = "1965002410";                   
-char token[] = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyMTU5In0.oldlOloegRoX_4FjidV5uoSQN1uCvKHnsBNZhkP0m2g";                               
-char server[] = "www.circusofthings.com";
-char statusDoLed_key[] = "18454";                           
-char modos_key [] = "26204";    
-char ligDes_key[] = "1476";
-char randNumber_key[] = "8819";                                            
-CircusESP32Lib circusESP32(server,ssid,password); // O objeto que representa um ESP32 para você poder solicitar gravação ou leitura
+char pass[] = "1965002410";                   
 
+/// Configurando portas virtuais Blynk App
+BLYNK_WRITE(V1){
+  switch (param.asInt()){
+    case 1: {
+      modoInteligente();
+      break;
+    }
+    case 2:{
+      modoFerias();
+      break;
+    }
+    case 3:{
+      modoManual();
+      break;
+    }
+  }
+}
+
+BLYNK_WRITE(V3){
+    manual_order = param.asInt(); 
+}
 
 /// Funções
-void modoInteligente()
-{
+void modoInteligente(){
   ldrValor = analogRead(ldrPin);
   if ((ldrValor>= 900) && (digitalRead(pinoPIR) == HIGH)){
-    statusLed = 1;
-    circusESP32.write(statusDoLed_key,statusLed,token);
+    statusLed.on();
     digitalWrite(ledPin, HIGH);
   }
   else {
-    statusLed = 0;
-    circusESP32.write(statusDoLed_key,statusLed,token);
+    statusLed.off();
     digitalWrite(ledPin, LOW);
   }
 }
 
-void modoFerias()
-{
+void modoFerias(){
   ldrValor = analogRead(ldrPin);
   randNumber = random(10, 100); // Numero aleatorio entre 10 e 99
-  circusESP32.write(randNumber_key,randNumber,token);
-  if (ldrValor>= 900 && randNumber >= 50) 
-  {
-    statusLed = 1;
-    circusESP32.write(statusDoLed_key,statusLed,token);
+  Blynk.virtualWrite(V2, randNumber);
+  if (ldrValor>= 900 && randNumber >= 50){
+    statusLed.on();
     digitalWrite(ledPin, HIGH);
     delay(3000);
   }
-  else 
-  {
-    statusLed = 0;
-    circusESP32.write(statusDoLed_key,statusLed,token);
+  else {
+    statusLed.off();
     digitalWrite(ledPin, LOW);
-    digitalWrite(ledPin2, LOW);
   }
 }
 
-void modoManual()
-{
-  double dashboard_order = circusESP32.read(ligDes_key, token);
-  if (dashboard_order == 1) 
-  {
-    digitalWrite(ledPin, HIGH);
-    statusLed = 1;
-    circusESP32.write(statusDoLed_key,statusLed,token);
-  }
-  else 
-  {
-    digitalWrite(ledPin, LOW);
-    statusLed = 0;
-    circusESP32.write(statusDoLed_key,statusLed,token);
-  }
+void modoManual(){
+  if (manual_order == 1) {
+      statusLed.on();
+      digitalWrite(ledPin, HIGH);
+    }
+    else {
+      statusLed.off();
+      digitalWrite(ledPin, LOW);
+    }
 }
 
 
-void setup() 
-{
+void setup() {
  pinMode(ledPin,OUTPUT); //define a ledPin como saída
  pinMode(ldrPin,INPUT); //define a ldrPin como saída
  pinMode(pinoPIR, INPUT); //define o pinoPIR como saida
  Serial.begin(115200); //Inicia a comunicação serial
- circusESP32.begin(); // Deixe o objeto Circuis configurar-se para uma conexão SSL / Secure
+ Blynk.begin(auth, ssid, pass);
 }
 
-
-void loop() 
-{
-  modoSelecionado = circusESP32.read(modos_key, token);
-  if (modoSelecionado == 0) modoInteligente();
-  else if (modoSelecionado == 1) modoFerias();
-  else modoManual();
+void loop(){
+  Blynk.run(); 
 }
